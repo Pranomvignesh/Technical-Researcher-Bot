b0c60eb3bf55d3acce2b4cd02e6f28189d9e775a
Pranomvignesh <pranomvignesh@gmail.com>
2023-10-02 03:00:40 -0400

    Initialized Chatbot

---
+++ pages/Documentation.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+st.set_page_config(
+    layout="wide",
+    initial_sidebar_state="expanded",
+    page_title="Technical Researcher Bot - Documentation",
+    page_icon="🤖",
+    menu_items={
+        'Report a bug': "https://github.com/Pranomvignesh/Technical-Researcher-Bot/issues"
+    }
+)
+st.title("Documentation")
